fda9947268567451fd9f15011406ea602bf983bf
Habasescu Alin Marian <habasescualinmarian@gmail.com>
2024-08-23 11:43:53 +0200

    Created a draft of the DirectedGraph classes with implementations using both adjacency matrix and adjacency list.

---
+++ DataStructures/Graph/HeaderFiles/DiGraph.h
@@ -0,0 +1,15 @@
+#ifndef _DIGRAPH_H
+#define _DIGRAPH_H
+#include "Graph.h"
+
+template <typename T>
+class DiAdjMatGrapgh : public AdjMatGraph<T>{
+    
+};
+
+template <typename T>
+class DiAdjListGrapgh : public AdjListGraph<T>{
+    
+};
+
+#endif
